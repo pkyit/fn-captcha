cb3e06e96287953c6e23d041907209e8b81d4921
pkyit <2660894933@qq.com>
2026-01-18 16:12:26 +0800

    git init

---
+++ src/main/java/com/github/pkyit/fncaptcha/FnCaptchaApplication.java
@@ -0,0 +1,13 @@
+package com.github.pkyit.fncaptcha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FnCaptchaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FnCaptchaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/pkyit/fncaptcha/FnCaptchaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.pkyit.fncaptcha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FnCaptchaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
